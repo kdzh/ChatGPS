b1b2a2aed1e26127918e773caad45b58bf2182a3
Konstantin Dzhigardzhyan <k.dzhigardzhyan@gmail.com>
2025-07-31 13:47:23 +0300

    update

--- app.py
+++ app.py
@@ -334,7 +334,7 @@ prompt_template = initial_prompt_template
 # st.sidebar.divider()
 
 st.sidebar.subheader("Параметры поисковика:")
-search_type = st.sidebar.selectbox('Тип поиска:', ('similarity', 'similarity_score_threshold', 'mmr'),)
+search_type = st.sidebar.selectbox('Тип поиска:', ('mmr', 'similarity', 'similarity_score_threshold'),)
 score_threshold = st.sidebar.slider('Пороговое значение оценки (только если тип поиска similarity_score_threshold):', 0.0, 1.0, 0.5)
 top_k_limit = st.sidebar.number_input(
     "Лимит на кол-во возвращаемых документов",
@@ -347,18 +347,18 @@ top_k_limit = st.sidebar.number_input(
 
 st.sidebar.subheader('Параметры векторной базы:')
 
-chunk_size = st.sidebar.number_input("Размер фрагмента:", min_value=100, max_value=10000, value=1000, step=1)
-chunk_overlap = st.sidebar.number_input("Прекрытие фрагментов:", min_value=1, max_value=1000, value=100, step=1)
+chunk_size = st.sidebar.number_input("Размер фрагмента:", min_value=100, max_value=10000, value=2000, step=1)
+chunk_overlap = st.sidebar.number_input("Прекрытие фрагментов:", min_value=1, max_value=1000, value=500, step=1)
 
 
 EMBEDDING_MODELS = [
-    "sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
     "sentence-transformers/LaBSE",
+    "sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
     "DeepPavlov/rubert-base-cased-sentence"
 ]
 
 embedding_model = st.sidebar.selectbox('Модель эмбеддингов:', tuple(EMBEDDING_MODELS),)
-db_type = st.sidebar.selectbox("Тип базы:", ("Chroma", "FAISS"))
+db_type = st.sidebar.selectbox("Тип базы:", ("FAISS", "Chroma"))
 
 st.session_state.vector_db = initialize_vector_db(
     chunk_size=chunk_size,
